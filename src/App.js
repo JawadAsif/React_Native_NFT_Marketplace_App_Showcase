1e519fbfcb582f5416e3bdf37b74f5747430e55d
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2022-05-15 23:05:20 +0600

    second section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Features, Download, SectionWrapper } from "./components";
+import assets from "./assets";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        React Native NFT Showcase
-      </h1>
+      <SectionWrapper
+        title="Your own store of nifty NFTs. 
+      Start selling and growing."
+        description="Buy, store, collect NFTs, exchange & earn crypto. 
+      Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace.
+         Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
     </div>
   );
 };
